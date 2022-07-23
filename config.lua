6773d41658f675d689ec5139cb06374dfaf48277
oosayeroo <98313914+oosayeroo@users.noreply.github.com>
2022-07-23 19:16:27 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,254 @@
+Config = {}
+
+--== EDIT THE PRICES TO WHATEVER YOU LIKE. I MADE THEM REALISTIC BUT YOU CAN CHANGE THEM==--
+
+--PROPS--
+Config.CoffeeMachineProp = {
+    690372739,
+}
+Config.SnackMachineProp = {
+    -654402915,
+	-1034034125,
+}
+Config.FizzyMachineProp = {
+    992069095,
+	1114264700,
+}
+Config.WaterMachineProp = {
+    1099892058,
+}
+
+--===ITEMS IN EACH MACHINE===-----   ADD WHATEVER YOU WANT
+Config.CoffeeItems = {
+    label = "Coffee Machine",
+        slots = 3,--slots must be same number as amount of items below
+        items = {
+            [1] = {
+                name = "regular-coffee",
+                price = 2,
+                amount = 5,
+                info = {},
+                type = "item",
+                slot = 1,
+            },
+            [2] = {
+                name = "latte-machiatto",
+                price = 3,
+                amount = 5,
+                info = {},
+                type = "item",
+                slot = 2,
+            },
+            [3] = {
+                name = "espresso",
+                price = 2,
+                amount = 5,
+                info = {},
+                type = "item",
+                slot = 3,
+            },
+        }
+    }
+    Config.SnackItems = {
+        label = "Snack Machine",
+            slots = 18,--slots must be same number as amount of items below
+            items = {
+                [1] = {
+                    name = "crinkle-habanero",
+                    price = 2,
+                    amount = 5,
+                    info = {},
+                    type = "item",
+                    slot = 1,
+                },
+                [2] = {
+                    name = "crinkle-paprika",
+                    price = 3,
+                    amount = 5,
+                    info = {},
+                    type = "item",
+                    slot = 2,
+                },
+                [3] = {
+                    name = "bigcheese-chips",
+                    price = 2,
+                    amount = 5,
+                    info = {},
+                    type = "item",
+                    slot = 3,
+                },
+                [4] = {
+                    name = "stickyribs-chips",
+                    price = 2,
+                    amount = 5,
+                    info = {},
+                    type = "item",
+                    slot = 4,
+                },
+                [5] = {
+                    name = "saltsauce-chips",
+                    price = 2,
+                    amount = 5,
+                    info = {},
+                    type = "item",
+                    slot = 5,
+                },
+                [6] = {
+                    name = "supersalt-chips",
+                    price = 2,
+                    amount = 5,
+                    info = {},
+                    type = "item",
+                    slot = 6,
+                },
+                [7] = {
+                    name = "zebrabar",
+                    price = 2,
+                    amount = 5,
+                    info = {},
+                    type = "item",
+                    slot = 7,
+                },
+                [8] = {
+                    name = "meteorite-bar",
+                    price = 2,
+                    amount = 5,
+                    info = {},
+                    type = "item",
+                    slot = 8,
+                },
+                [9] = {
+                    name = "salty-nothings",
+                    price = 2,
+                    amount = 5,
+                    info = {},
+                    type = "item",
+                    slot = 9,
+                },
+                [10] = {
+                    name = "captains-log",
+                    price = 2,
+                    amount = 5,
+                    info = {},
+                    type = "item",
+                    slot = 10,
+                },
+                [11] = {
+                    name = "candybox-red",
+                    price = 2,
+                    amount = 5,
+                    info = {},
+                    type = "item",
+                    slot = 11,
+                },
+                [12] = {
+                    name = "pnq-candybox",
+                    price = 2,
+                    amount = 5,
+                    info = {},
+                    type = "item",
+                    slot = 12,
+                },
+                [13] = {
+                    name = "candybox-cream",
+                    price = 2,
+                    amount = 5,
+                    info = {},
+                    type = "item",
+                    slot = 13,
+                },
+                [14] = {
+                    name = "candybox-green",
+                    price = 2,
+                    amount = 5,
+                    info = {},
+                    type = "item",
+                    slot = 14,
+                },
+                [15] = {
+                    name = "candybox-brown",
+                    price = 2,
+                    amount = 5,
+                    info = {},
+                    type = "item",
+                    slot = 15,
+                },
+                [16] = {
+                    name = "release-green",
+                    price = 2,
+                    amount = 5,
+                    info = {},
+                    type = "item",
+                    slot = 16,
+                },
+                [17] = {
+                    name = "release-pink",
+                    price = 2,
+                    amount = 5,
+                    info = {},
+                    type = "item",
+                    slot = 17,
+                },
+                [18] = {
+                    name = "release-blue",
+                    price = 2,
+                    amount = 5,
+                    info = {},
+                    type = "item",
+                    slot = 18,
+                },
+            }
+        }
+        Config.FizzyItems = {
+            label = "Drinks Machine",
+                slots = 4,--slots must be same number as amount of items below
+                items = {
+                    [1] = {
+                        name = "ecola",
+                        price = 2,
+                        amount = 5,
+                        info = {},
+                        type = "item",
+                        slot = 1,
+                    },
+                    [2] = {
+                        name = "sprunk",
+                        price = 3,
+                        amount = 5,
+                        info = {},
+                        type = "item",
+                        slot = 2,
+                    },
+                    [3] = {
+                        name = "orang-o-tang",
+                        price = 2,
+                        amount = 5,
+                        info = {},
+                        type = "item",
+                        slot = 3,
+                    },
+                    [4] = {
+                        name = "lemonade-can",
+                        price = 2,
+                        amount = 5,
+                        info = {},
+                        type = "item",
+                        slot = 4,
+                    },
+                }
+            }
+            Config.WaterItems = {
+                label = "Drinks Machine",
+                    slots = 4,--slots must be same number as amount of items below
+                    items = {
+                        [1] = {
+                            name = "raine-water",
+                            price = 2,
+                            amount = 5,
+                            info = {},
+                            type = "item",
+                            slot = 1,
+                        },
+                    }
+                }
+        
